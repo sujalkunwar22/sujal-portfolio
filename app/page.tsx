dfc1dc7624f539e2b31d9a2e5a94a56361fb7d5c
milan20252003 <xettrimilan5@gmail.com>
2026-05-12 02:07:15 +0545

    chore: remove unused footer import from home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
-import Footer from '@/components/Footer';
 import { CanvasBackground } from '@/components/ui/canvas-background';
 
 export default function Home() {
